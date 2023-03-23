1580eea535641c445302e80b62fe5209229e0d6a
Sanskar Modi <modisanskar5@gmail.com>
2023-03-23 16:11:59 +0530

    LoginUI Updated

--- lib/core/common/sign_in_button.dart
+++ lib/core/common/sign_in_button.dart
@@ -1,36 +1,31 @@
 import 'package:flutter/material.dart';
 
 import '../../theme/pallete.dart';
-import '../../widgets/custom_text.dart';
 import '../constants/constants.dart';
 
-class SignInButton extends StatefulWidget {
+class SignInButton extends StatelessWidget {
   const SignInButton({super.key});
 
   @override
-  State<SignInButton> createState() => _SignInButtonState();
-}
-
-class _SignInButtonState extends State<SignInButton> {
-  @override
   Widget build(BuildContext context) {
     return Padding(
-      padding: const EdgeInsets.symmetric(horizontal: 18.0),
+      padding: const EdgeInsets.all(18.0),
       child: ElevatedButton.icon(
-        label: const CustomText(
-          text: "Login with google",
-          fontSize: 18.0,
-        ),
+        onPressed: () {},
         icon: Image.asset(
           Constants.googlePath,
           width: 35,
         ),
-        onPressed: () {},
+        label: const Text(
+          'Continue with Google',
+          style: TextStyle(fontSize: 18),
+        ),
         style: ElevatedButton.styleFrom(
-            backgroundColor: Palette.greyColor,
-            minimumSize: const Size(double.infinity, 50),
-            shape: RoundedRectangleBorder(
-                borderRadius: BorderRadius.circular(30))),
+          backgroundColor: Palette.greyColor,
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(20),
+          ),
+        ),
       ),
     );
   }
--- lib/features/auth/screens/login_screen.dart
+++ lib/features/auth/screens/login_screen.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
+
 import '../../../core/common/sign_in_button.dart';
 import '../../../core/constants/constants.dart';
-import '../../../widgets/custom_text.dart';
 
 class LoginScreen extends StatelessWidget {
   const LoginScreen({super.key});
@@ -9,49 +9,46 @@ class LoginScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      // logo on appbar
       appBar: AppBar(
         title: Image.asset(
           Constants.logoPath,
           height: 40,
         ),
-        centerTitle: true,
         actions: [
           TextButton(
             onPressed: () {},
-            child: const CustomText(
-              text: "Skip",
-            ),
-          )
-        ],
-      ),
-      body: SingleChildScrollView(
-        child: Column(
-          children: [
-            const SizedBox(
-              height: 30,
-            ),
-            const CustomText(
-              text: "Dive into anything",
-              color: Colors.white,
-              fontSize: 24.0,
-            ),
-            const SizedBox(
-              height: 30,
-            ),
-            Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: Image.asset(
-                Constants.loginEmotePath,
-                height: 400,
+            child: const Text(
+              'Skip',
+              style: TextStyle(
+                fontWeight: FontWeight.bold,
               ),
             ),
-            const SizedBox(
-              height: 20,
+          ),
+        ],
+      ),
+      body: Column(
+        children: [
+          const SizedBox(height: 30),
+          const Text(
+            'Dive into anything',
+            style: TextStyle(
+                fontSize: 24, fontWeight: FontWeight.bold, letterSpacing: 0.5),
+          ),
+          const SizedBox(
+            height: 30,
+          ),
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Image.asset(
+              Constants.loginEmotePath,
+              height: 400,
             ),
-            const SignInButton()
-          ],
-        ),
+          ),
+          const SizedBox(
+            height: 20,
+          ),
+          const SignInButton(),
+        ],
       ),
     );
   }
