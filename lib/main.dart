806c1ae125473a173c7a977ae63f1a3e25589991
Sanskar Modi <modisanskar5@gmail.com>
2023-03-23 19:09:39 +0530

    Firebase Integration added

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,15 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:reddit_clone/features/auth/screens/login_screen.dart';
 
+import 'firebase_options.dart';
 import 'theme/pallete.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const MyApp());
 }
 
