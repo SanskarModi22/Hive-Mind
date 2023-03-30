60dc7fe05d7e666ac9ceecdd8cd59951ee89b193
Sanskar Modi <modisanskar5@gmail.com>
2023-03-30 22:21:01 +0530

    Storing Communities To Firestore

--- lib/core/typedef.dart
+++ lib/core/typedef.dart
@@ -3,3 +3,4 @@ import 'package:fpdart/fpdart.dart';
 import 'failure.dart';
 
 typedef FutureEither<T> = Future<Either<Failure, T>>;
+typedef FutureVoid = FutureEither<void>;
---
+++ lib/features/community/controller/community_controller.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:reddit_clone/core/constants/constants.dart';
+import 'package:reddit_clone/features/auth/controller/auth_controller.dart';
+import 'package:reddit_clone/features/community/repository/community_repository.dart';
+import 'package:routemaster/routemaster.dart';
+
+import '../../../core/utils.dart';
+import '../../../models/community_model.dart';
+
+class CommunityController extends StateNotifier<bool> {
+  final CommunityRepository _communityRepository;
+  final Ref _ref;
+  CommunityController(
+      {required CommunityRepository communityRepository, required Ref ref})
+      : _ref = ref,
+        _communityRepository = communityRepository,
+        super(false);
+
+  void createCommunity(String name, BuildContext context) async {
+    state = true;
+    final uid = _ref.read(userProvider)?.uid ?? '';
+    Community community = Community(
+      id: name,
+      name: name,
+      banner: Constants.bannerDefault,
+      avatar: Constants.avatarDefault,
+      members: [uid],
+      mods: [uid],
+    );
+    final res = await _communityRepository.createCommunity(community);
+    state = false;
+    res.fold((l) => showSnackBar(context: context, message: l.message), (r) {
+      showSnackBar(context: context, message: 'Community created successfully');
+      Routemaster.of(context).pop();
+    });
+  }
+}
