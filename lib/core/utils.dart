ceb6707d2e1b3cd787c65f1e6f0019cf7bd789d9
Sanskar Modi <modisanskar5@gmail.com>
2023-04-04 22:49:00 +0530

    Edit Community UI Done

--- lib/core/utils.dart
+++ lib/core/utils.dart
@@ -1,3 +1,4 @@
+import 'package:file_picker/file_picker.dart';
 import 'package:flutter/material.dart';
 
 void showSnackBar(
@@ -11,4 +12,9 @@ void showSnackBar(
       backgroundColor: color,
     ),
   );
+}
+
+Future<FilePickerResult?> pickImage() async {
+  final image = await FilePicker.platform.pickFiles(type: FileType.image);
+  return image;
 }
