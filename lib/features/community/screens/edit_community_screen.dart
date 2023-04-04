e772fb760c8fd07482f69850a7ae4a3cff32b546
Sanskar Modi <modisanskar5@gmail.com>
2023-04-04 22:38:12 +0530

    Eddit Community Screen UI Updated

--- lib/features/community/screens/edit_community_screen.dart
+++ lib/features/community/screens/edit_community_screen.dart
@@ -1,7 +1,13 @@
+import 'package:dotted_border/dotted_border.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
+import '../../../core/common/error_text.dart';
+import '../../../core/common/loader.dart';
+import '../../../core/constants/constants.dart';
+import '../../../theme/pallete.dart';
 import '../../../widgets/custom_text.dart';
+import '../controller/community_controller.dart';
 
 class EditCommunityScreen extends ConsumerStatefulWidget {
   final String name;
@@ -15,14 +21,45 @@ class EditCommunityScreen extends ConsumerStatefulWidget {
 class _EditCommunityScreenState extends ConsumerState<EditCommunityScreen> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        centerTitle: true,
-        title: const CustomText(text: "Edit Community"),
-        actions: [
-          TextButton(onPressed: () {}, child: const CustomText(text: "Save"))
-        ],
-      ),
-    );
+    return ref.watch(getCommunityByNameProvider(widget.name)).when(
+          data: (community) => Scaffold(
+            backgroundColor: Palette.darkModeAppTheme.colorScheme.background,
+            appBar: AppBar(
+              centerTitle: true,
+              title: const CustomText(text: "Edit Community"),
+              actions: [
+                TextButton(
+                    onPressed: () {}, child: const CustomText(text: "Save"))
+              ],
+            ),
+            body: Column(
+              children: [
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: DottedBorder(
+                      // radius: const Radius.circular(30),
+                      color: Colors.white,
+                      child: SizedBox(
+                        width: double.infinity,
+                        height: 150,
+                        child: community.banner.isEmpty ||
+                                community.banner == Constants.bannerDefault
+                            ? Center(
+                                child: IconButton(
+                                    onPressed: () {},
+                                    icon: const Icon(
+                                      Icons.camera_alt_outlined,
+                                      size: 40,
+                                    )),
+                              )
+                            : Image.network(community.banner),
+                      )),
+                )
+              ],
+            ),
+          ),
+          error: (error, stackTrace) => ErrorText(error: error.toString()),
+          loading: () => const Loader(),
+        );
   }
 }
